f2782a142d5f2bf327ca32328583de4d6102edae
Rehan <rehanalyze@gmail.com>
2025-07-01 00:52:19 +0530

    DAY 10 Finished, added try catch errors

--- App 1 - Todo List In Python/todo.py
+++ App 1 - Todo List In Python/todo.py
@@ -2,52 +2,58 @@ while True:
     user_action = input("Type add, show, complete, edit or exit: ")
     user_action = user_action.strip()
 
-    if 'add' in user_action:
-            with open("todo.txt", "r") as file:
-                todos = file.readlines()
-            
-            user_action = user_action[4:]
-            todos.append(user_action + "\n")
-            
-            with open("todo.txt", "w") as file:
-                todos = file.writelines(todos)
-    
-    elif 'show' in user_action:
+    if user_action.startswith("add"):
+        with open("todo.txt", "r") as file:
+            todos = file.readlines()
+
+        user_action = user_action[4:]
+        todos.append(user_action + "\n")
+
+        with open("todo.txt", "w") as file:
+            todos = file.writelines(todos)
+
+    elif user_action.startswith("show"):
         with open("todo.txt", "r") as file:
             todos = file.readlines()
 
         for index, item in enumerate(todos):
             print(f"{index + 1}. {item.strip()}")
 
-    elif 'edit' in user_action:
-        edit_number = int(user_action[5:])
-        edit_number = edit_number - 1
-        new_todo = input("Enter a new todo: ") + "\n"
-        
-        with open("todo.txt", "r") as file:
-            todos = file.readlines()
-        
-        todos[edit_number] = new_todo
+    elif user_action.startswith("edit"):
+        try:
+            edit_number = int(user_action[5:])
+            edit_number = edit_number - 1
+            new_todo = input("Enter a new todo: ") + "\n"
 
-        with open("todo.txt", "w") as file:
-            todos = file.writelines(todos)
+            with open("todo.txt", "r") as file:
+                todos = file.readlines()
 
-    elif 'complete' in user_action:
-        complete_number = int(user_action[9:])
-        complete_number = complete_number - 1
-        
-        with open("todo.txt", "r") as file:
-            todos = file.readlines()
-        
-        deleted = todos[complete_number]
-        todos.pop(complete_number)
+            todos[edit_number] = new_todo
 
-        with open("todo.txt", "w") as file:
-            todos = file.writelines(todos)
+            with open("todo.txt", "w") as file:
+                todos = file.writelines(todos)
+        except ValueError:
+            print("Invalid syntax dumbass")
 
-        print(f"Todo {deleted.strip()} is completed")
+    elif user_action.startswith("complete"):
+        try:
+            complete_number = int(user_action[9:])
+            complete_number = complete_number - 1
 
-    elif 'exit' in user_action:
+            with open("todo.txt", "r") as file:
+                todos = file.readlines()
+
+            deleted = todos[complete_number]
+            todos.pop(complete_number)
+
+            with open("todo.txt", "w") as file:
+                todos = file.writelines(todos)
+
+            print(f"Todo {deleted.strip()} is completed")
+        except (ValueError, IndexError):
+            print("Invalid syntax dumbass")
+
+    elif user_action.startswith("exit"):
         break
     else:
         print("Invalid syntax dumbass")
